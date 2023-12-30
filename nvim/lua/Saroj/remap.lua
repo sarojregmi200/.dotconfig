90d460d5bc4ab26ab00a1fd8640ffd739fa9e41f
Saroj Regmi <sarojregmi.official@gmail.com>
2023-12-30 22:37:49 +0545

    Updated wezterms config

--- nvim/lua/Saroj/remap.lua
+++ nvim/lua/Saroj/remap.lua
@@ -7,3 +7,7 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 -- remaping the esc to jk or kj
 vim.keymap.set("i", "kj", "<ESC>")
 vim.keymap.set("i", "jk", "<ESC>")
+
+-- remaping zoom out and zoom in
+vim.api.nvim_set_keymap("n", " zz", "<c-w>_", { noremap = true })
+vim.api.nvim_set_keymap("n", " zo", "<c-w>=", { noremap = true })
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -6,11 +6,23 @@ if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
+-- terminal essentials
+config.audible_bell = "Disabled" --beep sound
+
 -- setting the color theme
 config.color_scheme = "Catppuccin Mocha (Gogh)"
 
 -- making the background transparent
 config.window_background_opacity = 0.95
 config.enable_tab_bar = false
+
+--turning off some overlapping keybinds
+config.keys = {
+	{
+		key = "Enter",
+		mods = "ALT",
+		action = wezterm.action.DisableDefaultAssignment,
+	},
+}
 
 return config
