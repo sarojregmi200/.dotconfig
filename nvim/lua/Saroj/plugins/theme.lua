f9d4f40762502dd75c4d5014d0e71294e5b7f670
Saroj Regmi <sarojregmi.official@gmail.com>
2024-01-10 23:53:57 +0545

    Added lua line

--- nvim/lua/Saroj/plugins/theme.lua
+++ nvim/lua/Saroj/plugins/theme.lua
@@ -1,6 +1,4 @@
 return {
-	--    "catppuccin/nvim",
-	--   name = "catppuccin",
 	"morhetz/gruvbox",
 	name = "gruvbox",
 	priority = 1000,
@@ -9,6 +7,7 @@ return {
 		-- using the color theme.
 		local color = "gruvbox"
 		vim.cmd.colorscheme(color)
+
 		-- making the background transparent
 		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
