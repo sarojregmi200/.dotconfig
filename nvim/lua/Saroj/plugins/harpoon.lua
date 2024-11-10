7bcb5c07a281945c3ce8102849e1e9bd760b49d0
Saroj <sarojregmi.official@gmail.com>
2024-11-10 19:48:46 +0545

    chore(nvim): updated to harpoon 2

--- nvim/lua/Saroj/plugins/harpoon.lua
+++ nvim/lua/Saroj/plugins/harpoon.lua
@@ -1,23 +1,30 @@
 return {
-	"theprimeagen/harpoon",
+	"ThePrimeagen/harpoon",
+	branch = "harpoon2",
+	dependencies = { "nvim-lua/plenary.nvim" },
 	config = function()
-		local mark = require("harpoon.mark")
-		local ui = require("harpoon.ui")
+		local harpoon = require("harpoon")
+		harpoon:setup()
 
-		vim.keymap.set("n", "<leader>a", mark.add_file, { desc = "Add files to harpoon ui" })
-		vim.keymap.set("n", "<C-p>", ui.toggle_quick_menu, { desc = "Open harpoon Ui" })
+		vim.keymap.set("n", "<leader>a", function()
+			harpoon:list():add()
+		end, { desc = "Add files to harpoon ui" })
+
+		vim.keymap.set("n", "<C-p>", function()
+			harpoon.ui:toggle_quick_menu(harpoon:list())
+		end, { desc = "Open harpoon Ui" })
 
 		vim.keymap.set("n", "<C-h>", function()
-			ui.nav_file(1)
+			harpoon:list():select(1)
 		end, { desc = "Move to first file harpoon" })
 		vim.keymap.set("n", "<C-j>", function()
-			ui.nav_file(2)
+			harpoon:list():select(2)
 		end, { desc = "Move to second file harpoon" })
 		vim.keymap.set("n", "<C-k>", function()
-			ui.nav_file(3)
+			harpoon:list():select(3)
 		end, { desc = "Move to third file harpoon" })
 		vim.keymap.set("n", "<C-l>", function()
-			ui.nav_file(4)
+			harpoon:list():select(4)
 		end, { desc = "Move to Fourth file harpoon" })
 	end,
 }
