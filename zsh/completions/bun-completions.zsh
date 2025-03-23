c2c297e7738439f3c61caeae01b522488bd34d18
Saroj <sarojregmi.official@gmail.com>
2025-03-23 19:51:09 +0545

    feat: added some features.
    - Added history search with fzf
    - Added completion auto sourcing and seperated into a new directory.

---
+++ zsh/completions/bun-completions.zsh
@@ -0,0 +1,4 @@
+# bun completions
+[ -s "/home/saroj/.bun/_bun" ] && source "/home/saroj/.bun/_bun"
+
+. "$HOME/.local/bin/env"
---
+++ zsh/completions/loader.zsh
@@ -0,0 +1,5 @@
+for script in "$HOME/.config/.dotconfig/zsh/completions/"*; do
+    if [[ -f "$script" && $(basename "$script") != "loader.zsh" ]];then
+        source "$script"
+    fi
+done
